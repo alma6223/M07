860a63d3d1418da6b2c3aa9320a3962ef803a63f
Albert Martín Moreno <alma6223@office.proven.cat>
2023-10-01 15:35:13 +0000

    update

--- 02-10-23/7/functions.php
+++ 02-10-23/7/functions.php
@@ -1,4 +1,11 @@
 <?php
+    /**
+     * Converts an amount from Euro (EUR) to another currency.
+     *
+     * @param int $amount The amount to convert.
+     * @param string $currency The target currency code (e.g., 'usd', 'jpy').
+     * @return float The converted amount.
+     */
     function eur(int $amount, string $currency): float {
         if ($currency == 'usd') {
             return $amount * 0.94;
@@ -7,6 +14,13 @@
         }
     }
 
+    /**
+     * Converts an amount from US Dollar (USD) to another currency.
+     *
+     * @param int $amount The amount to convert.
+     * @param string $currency The target currency code (e.g., 'eur', 'jpy').
+     * @return float The converted amount.
+     */
     function usd(int $amount, string $currency): float {
         if ($currency == 'eur') {
             return $amount * 1.06;
@@ -15,6 +29,13 @@
         }
     }
 
+    /**
+     * Converts an amount from Japanese Yen (JPY) to another currency.
+     *
+     * @param int $amount The amount to convert.
+     * @param string $currency The target currency code (e.g., 'usd', 'eur').
+     * @return float The converted amount.
+     */
     function jpy(int $amount, string $currency): float {
         if ($currency == 'usd') {
             return $amount * 149.38;
@@ -22,3 +43,5 @@
             return $amount * 158.15;
         }
     }
+?>
+
