1809f8d786a4f2ba07da94376111b3bd5ab12f69
Albert Martín Moreno <alma6223@office.proven.cat>
2023-10-11 17:18:19 +0200

    cositas

--- 25-09-23/3.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function celsius(int $fahrenheit): int {
-        return ($fahrenheit - 32) / 1.8;
-    }
-
-    function fahrenheit(int $celsius): int {
-        return ($celsius * 1.8) + 32;
-    }
-?>
