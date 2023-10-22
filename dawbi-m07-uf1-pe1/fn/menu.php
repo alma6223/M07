2789966c3d274f134bee3b02ba9fa1170c9a106b
Albert Martín Moreno <alma6223@office.proven.cat>
2023-10-22 19:18:40 +0000

    finalizado

--- dawbi-m07-uf1-pe1/fn/menu.php
+++ dawbi-m07-uf1-pe1/fn/menu.php
@@ -3,6 +3,7 @@
  * get the categories from csv file
  * @param string $filename csv filename
  * @return array category array
+ * @return bool if the file does not exist
  */
 function categories(string $filename): array | bool {
     if (file_exists($filename)) {
@@ -21,13 +22,20 @@ function categories(string $filename): array | bool {
  * get the menu from csv file
  * @param string $filename csv filename
  * @return array menus array
+ * @return bool if the file does not exist
  */
 function menu(string $filename): array | bool {
     $menu = array();
     if (file_exists($filename)) {
         $stream = fopen($filename, 'r');
-        while ($dish = fgetcsv($stream, separator: ';')) {
-            array_push($menu, array('id' => $dish[0], 'category' => $dish[1], 'name' => $dish[2], 'price' => $dish[3]));
+		while ($data = fgetcsv($stream, separator: ';')) {
+			$dish = array(
+				'id' => $data[0],
+				'category' => $data[1],
+				'name' => $data[2],
+			   	'price' => $data[3]
+			);
+			array_push($menu, $dish);
         }
         fclose($stream);
         return $menu;
@@ -35,31 +43,33 @@ function menu(string $filename): array | bool {
         return false;
     }
 }
-
+		
 
 /**
  * shows menus according to their category
- * @param string $category menu category
+ * @param array $categories categories array
  * @param array $menu menus array
  */
-function table(string $category, array $menu): void {
-    echo "
-    <table>
-        <caption>" . ucwords($category) . "</caption>
-        <tr>
-            <th>Name</th>
-            <th>Price</th>
-        </tr>
-    ";
-    foreach($menu as $dish) {
-        if ($dish['category'] == $category) {
-            echo "     
-            <tr>
-                <td>{$dish['name']}</th>
-                <td>{$dish['price']}</th>
-            </tr>
-            ";
-        }
-    }
-    echo "</table>";
+function table(array $categories, array $menu): void {
+	foreach($categories as $category) {
+		echo "
+    		<table>
+        		<caption>" . ucwords($category) . "</caption>
+        		<tr>
+           			<th>Name</th>
+           	 		<th>Price</th>
+       			</tr>
+   	 	";
+    	foreach($menu as $dish) {
+        	if ($dish['category'] == $category) {
+            	echo "     
+            		<tr>
+                		<td>{$dish['name']}</th>
+                		<td>{$dish['price']}</th>
+            		</tr>
+            	";
+        	}
+   		}
+    	echo "</table>";
+	}
 }
--- dawbi-m07-uf1-pe1/fn/users.php
+++ dawbi-m07-uf1-pe1/fn/users.php
@@ -1,27 +1,41 @@
 <?php
 /**
- * 
+ * checks if the user does not exist and adds it to the file
+ * @param string $filename csv filename
+ * @param array $user user data
+ * @return bool register status 
  */
 function register(string $filename, array $user): bool {
-    $register = false;
+    $register = true;
     $stream = fopen($filename, 'r+');
     while ($data = fgetcsv($stream, separator: ';')) {
-        if ($data[0] != $user['username']) $register = true;
-    }
-    if ($register) fwrite($stream, $user['username'] . ';' . $user['password'] . ';' . $user['rol'] . ';' . $user['name'] . ';' . $user['surname'] . PHP_EOL); 
+		if ($data[0] == $user['username']) {
+			$register = false;
+		}
+	}
+	if ($register) {
+		$string = $user['username'] . ';' . $user['password'] . ';' . $user['rol'] . ';' . $user['name'] . ';' . $user['surname'] . PHP_EOL;
+		fwrite($stream, $string);
+	}
     fclose($stream);
     return $register;
 }
 
 
 /**
- * 
+ * checks the file if the username and password are correct
+ * @param string $filename csv file
+ * @param string $username
+ * @param string $password
+ * @return bool login status
  */
 function login(string $filename, string $username, string $password): bool {
     $login = false;
     $stream = fopen($filename, 'r');
     while ($data = fgetcsv($stream, separator: ';')) {
-        if ($data[0] == $username && $data[1] == $password) $login = true;
+		if ($data[0] == $username && $data[1] == $password) {
+			$login = true;
+		}
     }
     fclose($stream);
     return $login;
@@ -29,18 +43,23 @@ function login(string $filename, string $username, string $password): bool {
 
 
 /**
- * 
+ * obtains an array with all the user data
+ * @param string $filename csv filename
+ * @param string $username
+ * @return array user data
  */
 function user(string $filename, string $username): array {
     $stream = fopen($filename, 'r');
     while ($data = fgetcsv($stream, separator: ';')) {
-        if ($data[0] == $username) $user = array(
-            'username' => $data[0],
-            'password' => $data[1],
-            'rol' => $data[2],
-            'name' => $data[3],
-            'surname' => $data[4]
-        ); 
+        if ($data[0] == $username) {
+			$user = array(
+				'username' => $data[0],
+            	'password' => $data[1],
+            	'rol' => $data[2],
+            	'name' => $data[3],
+            	'surname' => $data[4]
+			);
+		} 
     }
     fclose($stream);
     return $user;
